80bab20ceb6328c0a14ba694c3731e52df31bed5
Pascal Charpentier <charpentier_pasc@hotmail.com>
2020-11-30 01:09:22 -0500

    Création de CreerPersonne: abstraite

---
+++ ControleurDeListeElectorale/creerpersonne.h
@@ -0,0 +1,116 @@
+#ifndef CREERPERSONNE_H
+#define CREERPERSONNE_H
+
+#include <QtGui>
+#include "Personne.h"
+
+class CreerPersonne : public QDialog
+{
+	enum Mode {Pret, EnCours, Initial};
+
+    Q_OBJECT
+
+public:
+    CreerPersonne(QWidget *parent = 0);
+    ~CreerPersonne();
+
+    virtual elections::Personne* reqPersonne() = 0; // Cette méthode permet à l'appelant de récupérer l'objet créé
+
+private slots:
+    void clicBoutonSoumettre();
+    void clicBoutonAnnuler();
+    void clicBoutonReInitialiser();
+    void formulaireModifie();
+
+private:
+
+    Mode modeCourant;
+
+    std::string nomAccepte;
+    std::string prenomAccepte;
+    std::string nasAccepte;
+
+    int jourAccepte;
+    int moisAccepte;
+    int anneeAcceptee;
+
+    int numCivicAccepte;
+    std::string nomRueAccepte;
+    std::string villeAcceptee;
+    std::string codePostalAccepte;
+    std::string provinceAcceptee;
+
+    /* Widgets du formulaire */
+
+    QPushButton* boutonSoumettre;
+    QPushButton* boutonAnnuler;
+    QPushButton* boutonReInitialiser;
+
+    QLabel* nomBanniere;
+    QLabel* prenomBanniere;
+    QLabel* nasBanniere;
+    QLabel* ddnBanniere;
+    QLabel* numeroCiviqueBanniere;
+    QLabel* nomRueBanniere;
+    QLabel* villeBanniere;
+    QLabel* codePostalBanniere;
+    QLabel* provinceBanniere;
+
+    QLineEdit* nomSaisie;
+    QLineEdit* prenomSaisie;
+    QLineEdit* nasSaisie;
+    QDateEdit* ddnSaisie;
+    QLineEdit* numeroCiviqueSaisie;
+    QLineEdit* nomRueSaisie;
+    QLineEdit* villeSaisie;
+    QLineEdit* codePostalSaisie;
+    QComboBox* provinceSaisie;
+
+    QLabel* nomValidation;
+    QLabel* prenomValidation;
+    QLabel* nasValidation;
+    QLabel* ddnValidation;
+    QLabel* numeroCiviqueValidation;
+    QLabel* nomRueValidation;
+    QLabel* villeValidation;
+    QLabel* codePostalValidation;
+    QLabel* provinceValidation;
+
+    QHBoxLayout* zoneBoutons;
+    QVBoxLayout* zoneSaisies;
+    QVBoxLayout* zoneBannieres;
+    QVBoxLayout* zoneValidations;
+
+    QHBoxLayout* zoneSuperieure;
+    QGridLayout* zoneGlobale;
+
+    /* Méthode de changement de mode */
+
+    void actualiserBoutons();
+    void actualiserSaisies();
+    void actualiserMessageDeValidation(QLabel*, bool);
+    void actualiserValidations();
+    void actualiser(Mode mode);
+
+    /* Validation des saisies */
+
+    bool lesSaisiesSontToutesValides();
+    bool laSaisieNomEstValide();
+    bool laSaisiePrenomEstValide();
+    bool laSaisieNasEstValide();
+    bool laSaisieDateEstValide();
+    bool laSaisieAdresseEstValide();
+
+    /* État initial */
+
+    void initialiserLesValeursAcceptees();
+    void initialiserBoutons();
+    void initialiserBannieres();
+    void initialiserZonesDeSaisie();
+    void initialiserZonesDeValidation();
+    void miseEnPageDuFormulaire();
+
+};
+
+
+#endif // CREERPERSONNE_H
